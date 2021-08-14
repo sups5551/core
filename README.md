47998267451aa2970dedb52a37206c6774fe8deb
sups5551 <87723792+sups5551@users.noreply.github.com>
2021-08-14 13:57:49 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# core
+# Correlation-data-set
---
+++ core_relation.py
@@ -0,0 +1,6 @@
+import plotly.express as px
+import csv
+with open('ice.csv') as csv_file:
+    df=csv.DictReader(csv_file)
+    fig=px.scatter(df,x='Temperature',y='Ice-cream Sales')
+    fig.show
